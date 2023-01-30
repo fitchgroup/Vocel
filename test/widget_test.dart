1f18f3ac2cd0bc5200db1a3e054f1d8890912863
Derek Ferguson <derekmferguson@yahoo.co.uk>
2023-01-30 15:13:08 -0600

    Fixed test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vocel/main.dart';
+import 'package:vocel/simple_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(isAmplifySuccessfullyConfigured: true));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
