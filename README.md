7be6ffdf525d7d90c5d0f8b1a2a7d3a2abf1ae5c
Derek Ferguson <derekmferguson@yahoo.co.uk>
2023-01-30 14:12:11 -0600

    Adding a generic Amplify Configuration file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Vocel
 Open source communications app for Vocel
+
+To use, replace these constants in amplifyconfiguration.dart...
+
+COGNITO_CREDS_POOL_ID
+COGNITO_CREDS_REGION
+COGNITO_USER_POOL_ID
+COGNITO_USER_APP_CLIENT_ID
+COGNITO_USER_REGION
---
+++ amplifyconfiguration.dart
@@ -0,0 +1,53 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "COGNITO_CREDS_POOL_ID",
+                            "Region": "COGNITO_CREDS_REGION"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "COGNITO_USER_POOL_ID",
+                        "AppClientId": "COGNITO_USER_APP_CLIENT_ID",
+                        "Region": "COGNITO_USER_REGION"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [
+                            "EMAIL"
+                        ],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
