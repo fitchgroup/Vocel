c42e90364648fb83c5511d384b4efbeb30657c7a
pyangyu <89769081+pyangyu@users.noreply.github.com>
2023-06-13 11:48:54 -0500

    changing dependencies from amplify flutter v0 to flutter v1 reference to Amplify Dev Center

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,29 +5,31 @@ export type AmplifyDependentResourcesAttributes = {
       "ApiName": "string",
       "RootUrl": "string"
     },
-    "fitchvocel": {
+    "vocelapp": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string"
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
     }
   },
   "auth": {
-    "fitchvocel5266d294": {
+    "userPoolGroups": {
+      "BELLGroupRole": "string",
+      "EETCGroupRole": "string",
+      "STAFFGroupRole": "string",
+      "VCPAGroupRole": "string"
+    },
+    "vocelappcf1453cb": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
   },
   "function": {
-    "AdminQueriesfd66d56a": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "fitchvocel5266d294PostConfirmation": {
+    "AdminQueries5b1ab4e5": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
@@ -36,7 +38,7 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "storage": {
-    "s392ab7284": {
+    "s37710959f": {
       "BucketName": "string",
       "Region": "string"
     }
