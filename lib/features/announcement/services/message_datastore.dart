49a6b69eb2c8a3c44131f898bbad0cc82bb0042c
pyangyu <89769081+pyangyu@users.noreply.github.com>
2023-07-03 11:23:26 -0500

    add direct message model

---
+++ lib/features/announcement/services/message_datastore.dart
@@ -0,0 +1,93 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:vocel/models/VocelMessage.dart';
+
+final messagesDataStoreServiceProvider =
+    Provider<MessagesDataStoreService>((ref) {
+  final service = MessagesDataStoreService();
+  return service;
+});
+
+class MessagesDataStoreService {
+  MessagesDataStoreService();
+
+  Stream<List<VocelMessage>> listenToMessages() {
+    return Amplify.DataStore.observeQuery(
+      VocelMessage.classType,
+      sortBy: [VocelMessage.CREATEDTIME.ascending()],
+    )
+        .map((message) => message.items
+            .where((element) =>
+                element.createdAt!.getDateTimeInUtc().isAfter(DateTime.now()))
+            .toList())
+        .handleError(
+      (error) {
+        debugPrint('listenToMessages: A Stream error happened');
+      },
+    );
+  }
+
+  Stream<List<VocelMessage>> listenToPastMessages() {
+    return Amplify.DataStore.observeQuery(
+      VocelMessage.classType,
+      sortBy: [VocelMessage.CREATEDTIME.ascending()],
+    )
+        .map((event) => event.items
+            .where((element) =>
+                element.createdAt!.getDateTimeInUtc().isBefore(DateTime.now()))
+            .toList())
+        .handleError(
+      (error) {
+        debugPrint('listenToMessages: A Stream error happened');
+      },
+    );
+  }
+
+  Stream<VocelMessage> getMessageStream(String id) {
+    final messageStream = Amplify.DataStore.observeQuery(VocelMessage.classType,
+            where: VocelMessage.ID.eq(id))
+        .map((message) => message.items.toList().single);
+
+    return messageStream;
+  }
+
+  Future<void> addMessage(VocelMessage event) async {
+    try {
+      await Amplify.DataStore.save(event);
+    } on Exception catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+
+  Future<void> deleteMessage(VocelMessage event) async {
+    try {
+      await Amplify.DataStore.delete(event);
+    } on Exception catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+
+  Future<void> updateMessage(VocelMessage updatedMessage) async {
+    try {
+      final messagesWithId = await Amplify.DataStore.query(
+        VocelMessage.classType,
+        where: VocelMessage.ID.eq(updatedMessage.id),
+      );
+
+      final oldMessage = messagesWithId.first;
+      final newMessage = oldMessage.copyWith(
+        content: updatedMessage.content,
+        messageImageUrl: updatedMessage.messageImageUrl,
+        messageImageKey: updatedMessage.messageImageKey,
+        attachedLink: updatedMessage.attachedLink,
+        sender: updatedMessage.sender,
+        receiver: updatedMessage.receiver,
+      );
+
+      await Amplify.DataStore.save(newMessage);
+    } on Exception catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+}
