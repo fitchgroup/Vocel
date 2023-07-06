efba8d61e84319abdf1817b307764d93afc56eda
pyangyu <89769081+pyangyu@users.noreply.github.com>
2023-07-06 15:46:22 -0500

    update auth

--- lib/common/repository/auth_inject.dart
+++ lib/common/repository/auth_inject.dart
@@ -2,7 +2,6 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:vocel/amplifyconfiguration.dart';
 import 'package:vocel/common/repository/auth_repository.dart';
 import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_datastore/amplify_datastore.dart';
@@ -38,7 +37,7 @@ class configureAmplifySuccess extends AuthRepository {
       ]);
 
       const String amplifyConfig = String.fromEnvironment('VERSION');
-      await Amplify.configure(amplifyconfig);
+      await Amplify.configure(amplifyConfig);
       return "Successfully configured";
     } on Exception catch (e) {
       debugPrint('${"=" * 50}\nError configuring Amplify: $e\n${"=" * 50}');
